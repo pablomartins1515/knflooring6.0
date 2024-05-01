d34594f6d460bd23561a6742ff93a1c42aabc83f
Pablo Martins <pablomartins1515@hotmail.com>
2024-05-01 11:18:23 -0300

    Add files via upload

--- src/components/Subscribe.tsx
+++ src/components/Subscribe.tsx
@@ -89,12 +89,12 @@ const Subscribe: React.FC<Props> = ({ token }) => {
     const handlePhone = (event: React.ChangeEvent<HTMLInputElement>) => {
         let value = event.target.value;
         if (value) {
-            value = value.replace(/\D/g, '');
-            value = value.replace(/(\d{2})(\d)/, "($1) $2");
-            value = value.replace(/(\d)(\d{4})$/, "$1-$2");
+            value = value.replace(/(\d{3})(\d{3})(\d{0,4})/, "($1) $2-$3");
         }
         setWhatsapp(value);
     };
+    
+    
 
     const handlePhone2 = (event: React.ChangeEvent<HTMLInputElement>) => {
         let value = event.target.value;
@@ -114,23 +114,30 @@ const Subscribe: React.FC<Props> = ({ token }) => {
         <div>
             <section id="idScrollComponentButtonGetAQuote">
                 <div className="pt-16 flex justify-center items-center">
+
                     <form onSubmit={handleSubscribe} className="flex flex-col gap-4 px-4 w-96 justify-center">
                         <input
-                            className="bg-zinc-200 text-gray-900 rounded-md px-5 h-14 border border-gray-400"
+                            className="bg-white text-gray-900 rounded-md px-5 h-14 border border-gray-400"
                             type="text"
                             placeholder="Name"
                             onChange={event => setName(event.target.value)}
                         />
 
-                        <input
-                            className="bg-zinc-200 text-gray-900 rounded-md px-5 h-14 border border-gray-400"
-                            type="tel"
-                            maxLength={15}
-                            placeholder="Tel or WhatsApp"
-                            value={whatsapp}
-                            onChange={event => handlePhone(event)}
-                        />
+                        <div className=' flex'>
+                            <div className=" rounded-s-lg gap-1 h-14 p-4 flex-shrink-0  inline-flex items-center py-2.5   text-center text-gray-900 bg-white border-s border-t border-b border-gray-400  hover:bg-gray-50 focus:ring-4 focus:outline-none focus:ring-gray-50 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600">
+                                <svg fill="none" aria-hidden="true" className="h-4 w-5 me-2" viewBox="0 0 20 15"><rect width="19.6" height="14" y=".5" fill="#fff" rx="2" /><mask id="a" width="20" height="15" x="0" y="0" maskUnits="userSpaceOnUse"><rect width="19.6" height="14" y=".5" fill="#fff" rx="2" /></mask><g mask="url(#a)"><path fill="#D02F44" fill-rule="evenodd" d="M19.6.5H0v.933h19.6V.5zm0 1.867H0V3.3h19.6v-.933zM0 4.233h19.6v.934H0v-.934zM19.6 6.1H0v.933h19.6V6.1zM0 7.967h19.6V8.9H0v-.933zm19.6 1.866H0v.934h19.6v-.934zM0 11.7h19.6v.933H0V11.7zm19.6 1.867H0v.933h19.6v-.933z" clip-rule="evenodd" /><path fill="#46467F" d="M0 .5h8.4v6.533H0z" /><g filter="url(#filter0_d_343_121520)"><path fill="url(#paint0_linear_343_121520)" fill-rule="evenodd" d="M1.867 1.9a.467.467 0 11-.934 0 .467.467 0 01.934 0zm1.866 0a.467.467 0 11-.933 0 .467.467 0 01.933 0zm1.4.467a.467.467 0 100-.934.467.467 0 000 .934zM7.467 1.9a.467.467 0 11-.934 0 .467.467 0 01.934 0zM2.333 3.3a.467.467 0 100-.933.467.467 0 000 .933zm2.334-.467a.467.467 0 11-.934 0 .467.467 0 01.934 0zm1.4.467a.467.467 0 100-.933.467.467 0 000 .933zm1.4.467a.467.467 0 11-.934 0 .467.467 0 01.934 0zm-2.334.466a.467.467 0 100-.933.467.467 0 000 .933zm-1.4-.466a.467.467 0 11-.933 0 .467.467 0 01.933 0zM1.4 4.233a.467.467 0 100-.933.467.467 0 000 .933zm1.4.467a.467.467 0 11-.933 0 .467.467 0 01.933 0zm1.4.467a.467.467 0 100-.934.467.467 0 000 .934zM6.533 4.7a.467.467 0 11-.933 0 .467.467 0 01.933 0zM7 6.1a.467.467 0 100-.933.467.467 0 000 .933zm-1.4-.467a.467.467 0 11-.933 0 .467.467 0 01.933 0zM3.267 6.1a.467.467 0 100-.933.467.467 0 000 .933zm-1.4-.467a.467.467 0 11-.934 0 .467.467 0 01.934 0z" clip-rule="evenodd" /></g></g><defs><linearGradient id="paint0_linear_343_121520" x1=".933" x2=".933" y1="1.433" y2="6.1" gradientUnits="userSpaceOnUse"><stop stop-color="#fff" /><stop offset="1" stop-color="#F0F0F0" /></linearGradient><filter id="filter0_d_343_121520" width="6.533" height="5.667" x=".933" y="1.433" color-interpolation-filters="sRGB" filterUnits="userSpaceOnUse"><feFlood flood-opacity="0" result="BackgroundImageFix" /><feColorMatrix in="SourceAlpha" result="hardAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" /><feOffset dy="1" /><feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.06 0" /><feBlend in2="BackgroundImageFix" result="effect1_dropShadow_343_121520" /><feBlend in="SourceGraphic" in2="effect1_dropShadow_343_121520" result="shape" /></filter></defs></svg>
+                                <p className=''>+ 1 </p>
+                            </div>
 
+                            <input
+                                className="bg-zinc-200 text-gray-900 px-5 h-14 border border-gray-400 rounded-e-md w-96"
+                                type="tel"
+                                maxLength={14}
+                                placeholder="Tel or WhatsApp"
+                                value={whatsapp}
+                                onChange={event => handlePhone(event)}
+                            />
+                        </div>
                         <input
                             className="flex bg-zinc-200 text-gray-900 rounded-md px-5 h-28 border border-gray-400"
                             type="text"
@@ -138,7 +145,7 @@ const Subscribe: React.FC<Props> = ({ token }) => {
                             onChange={event => setMessage(event.target.value)}
                         />
 
-                        <input 
+                        <input
                             id="whatsappAdm"
                             className="bg-zinc-200 text-gray-900 rounded px-5  h-0 "
                             type="tel"
