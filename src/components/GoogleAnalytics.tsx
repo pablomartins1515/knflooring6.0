5b6db9ce5cbcca78205ebdf1a24e91787aa1db04
Pablo Martins <pablomartins1515@hotmail.com>
2024-05-06 14:22:50 -0300

    Update GoogleAnalytics.tsx

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -1,22 +1,22 @@
 import React from "react";
 import Script from 'next/script';
 
-// este compoente não está utilizado ainda. Só o do index principal no doc do pages
-
 const GoogleAnalytics = () => {
   return (
     <>
-      {/* Google Analytics */}
-      <!-- Google tag (gtag.js) -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16549865575">
-        </script>
-          <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-          
-            gtag('config', 'AW-16549865575');
-          </script>
+      {/* Google tag (gtag.js) */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=AW-16549865575"
+        strategy="afterInteractive"
+      />
+      <Script strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-16549865575');
+        `}
+      </Script>
       {/* End Google Analytics */}
     </>
   );
